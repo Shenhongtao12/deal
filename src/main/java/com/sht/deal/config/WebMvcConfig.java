7206f3a6115b39f18f250db43c603059d1cf6fb0
hongtao.shen <hongtao.shen@rootcloud.com>
2020-06-22 15:44:32 +0800

    WebMvcConfig

---
+++ src/main/java/com/sht/deal/config/WebMvcConfig.java
@@ -0,0 +1,45 @@
+package com.sht.deal.config;
+
+import com.sht.deal.interceoter.LoginIntercepter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * 拦截器配置
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginIntercepter())
+                .addPathPatterns("/api/token/*/**")
+                //配置不拦截的api
+                //.excludePathPatterns("/ring/user/login","/ring/user/init","/ring/user/getToken")
+        ;
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    /**
+     * 跨域处理
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+    /**
+     * 静态资源映射配置
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/deal/**").addResourceLocations("file:/deal/");
+    }
+}
